43c9b5931d66a8e063ab6a8bc0a0e687b20fe7e0
idooooo <352387963@qq.com>
2019-09-01 23:37:18 +0800

    modify rl

--- add/php/additem.php
+++ add/php/additem.php
@@ -8,14 +8,14 @@ $id = $_GET["id"];
 if( $phoneid && $name && $id ){
 
     // 从文件中读取数据到PHP变量  
-    $json_phoneid = file_get_contents('./phoneid.json');  
+    $json_phoneid = file_get_contents('../phoneid.json');  
     // 用参数true把JSON字符串强制转成PHP数组  
     $array_phoneid = json_decode($json_phoneid, true); 
 
     if(!in_array($phoneid, $array_phoneid["phoneid"])){
         echo $phoneid . "用户未添加kay, 访问/adduser.html 进行添加";
     }else{
-        $json_config = file_get_contents('./config/' . $phoneid . '_config.json');
+        $json_config = file_get_contents('../config/' . $phoneid . '_config.json');
         $config = json_decode($json_config, true); 
 
         $item_flag = FALSE;
@@ -29,7 +29,7 @@ if( $phoneid && $name && $id ){
             array_push($config["post_list"], array('description'=>$name, 'id'=>$id));
 
             $json_string = json_encode($config, JSON_UNESCAPED_UNICODE);
-            file_put_contents('./config/' . $phoneid . '_config.json', $json_string);
+            file_put_contents('../config/' . $phoneid . '_config.json', $json_string);
 
 
             echo $name . "的运单号为:" . $id . "添加成功<br><br><br>";
