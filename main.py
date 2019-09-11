22fdb69315c8d087b0166a7ea5262118bbf4fcb1
ooooodi <ooooodi@qq.com>
2019-09-11 20:35:54 +0800

    write company into config file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import requests
 import re
 
-from logger import logger
+# from logger import logger
 from handle import handle_data, handle_result
 from proxy import detail_proxy, belong_to_proxy
 from info import get_all_user_phone, get_one_user_config, get_one_user_data, set_one_user_data,set_one_user_config
@@ -40,20 +40,21 @@ def get_express_from_id(id):
 
 
 def send(phoneid):
-
     data = get_one_user_data(phoneid)
     config = get_one_user_config(phoneid)
+    config_hash = hash(str(config))
 
     data_new = {}
     key = config.get("key")
-    for i in config.get("post_list"):
-        id = i.get('id')
-        company = i.get('company')
+    items = config.get("items")
+    for id,info in items.items():
+        company = info.get('company')
         if not company:
             company = get_express_from_id(id)
-        description = i.get('description')
+            config["items"]["id"]["company"] = company
+        description = info.get('description')
         if not id or not description:
-            print('\nconfig.py文件错误！', i, '\n')
+            print('\nconfig.py文件错误！', id, '\n')
             return
 
         # 如果是一个新的数据
@@ -83,11 +84,11 @@ def send(phoneid):
         time.sleep(1)
 
     set_one_user_data(phoneid, data_new)
-    # if not config == config_new:
-    #     set_one_user_config(phoneid,config_new)
+    if not config_hash == hash(str(config)):
+        set_one_user_config(phoneid,config)
 
-ids = get_all_user_phone()
-for phoneid in ids.get("phoneid"):
+phoneids = get_all_user_phone()
+for phoneid in phoneids.get("phoneid"):
     print("+ = + = + = {t} = + = + ={phoneid}+ = + = + = {t} = + = + =".format(
         phoneid=phoneid, t=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
     send(phoneid)
