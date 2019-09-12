a66c6ba4ca075f0c1cbde85bad24de3dcbf0e312
ooooodi <ooooodi@qq.com>
2019-09-12 17:28:48 +0800

    modify php : add  enpty file control

--- add/php/additem.php
+++ add/php/additem.php
@@ -7,10 +7,15 @@ $id = $_GET["id"];
 
 if( $phoneid && $name && $id ){
 
-    // 从文件中读取数据到PHP变量  
-    $json_phoneid = file_get_contents('../../phoneid.json');  
-    // 用参数true把JSON字符串强制转成PHP数组  
-    $array_phoneid = json_decode($json_phoneid, true); 
+    if(!file_exists('../../phoneid.json')){
+        echo "请先添加用户";
+        die();
+    }else{
+        // 从文件中读取数据到PHP变量  
+        $json_phoneid = file_get_contents('../../phoneid.json');  
+        // 用参数true把JSON字符串强制转成PHP数组  
+        $array_phoneid = json_decode($json_phoneid, true);
+    }
 
     if(!in_array($phoneid, $array_phoneid["phoneid"])){
         echo $phoneid . "用户未添加kay, 访问/adduser.html 进行添加";
@@ -18,15 +23,8 @@ if( $phoneid && $name && $id ){
         $json_config = file_get_contents('../../config/' . $phoneid . '_config.json');
         $config = json_decode($json_config, true); 
 
-        $item_flag = FALSE;
-        foreach($config["post_list"] as $item){
-            if($item["id"] == $id){
-                $item_flag = TRUE;
-                break;
-            }
-        }
-        if(!$item_flag){
-            array_push($config["post_list"], array('description'=>$name, 'id'=>$id));
+        if(!$config["items"][$id]){
+            $config["items"][$id] = array('description'=>$name, 'company'=>"");
 
             $json_string = json_encode($config, JSON_UNESCAPED_UNICODE);
             file_put_contents('../../config/' . $phoneid . '_config.json', $json_string);
@@ -34,8 +32,8 @@ if( $phoneid && $name && $id ){
 
             echo $name . "的运单号为:" . $id . "添加成功<br><br><br>";
             echo "目前存在的快递有:<br>";
-            foreach($config["post_list"] as $item){
-                echo "<br>名字：" . $item["description"] . "---运单号：" . $item["id"];
+            foreach($config["items"] as $k => $v){
+                echo "<br>名字：" . $v["description"] . "---运单号：" . $k;
             }
         }else{
             echo "运单号:" . $id . "已经存在了";
--- add/php/adduser.php
+++ add/php/adduser.php
@@ -5,30 +5,47 @@ $phoneid = $_GET["phoneid"];
 $key = $_GET["key"];
 
 if($phoneid && $key){
-    // 从文件中读取数据到PHP变量  
-    $json_phoneid = file_get_contents('../../phoneid.json');  
-    // 用参数true把JSON字符串强制转成PHP数组  
-    $array_phoneid = json_decode($json_phoneid, true); 
+    if (file_exists('../../phoneid.json')){
+        // 从文件中读取数据到PHP变量  
+        $json_phoneid = file_get_contents('../../phoneid.json');
 
-    if(!in_array($phoneid, $array_phoneid["phoneid"])){
-        array_push($array_phoneid["phoneid"], $phoneid);
-        file_put_contents('../../phoneid.json', json_encode($array_phoneid, JSON_UNESCAPED_UNICODE));
+        // 用参数true把JSON字符串强制转成PHP数组 
+        $array_phoneid = json_decode($json_phoneid, true);
+        if(!in_array($phoneid, $array_phoneid["phoneid"])){
+            array_push($array_phoneid["phoneid"], $phoneid);
 
-        $config = array("key" => $key,"post_list" =>array(array("description" => "首次添加测试","id" => "75168316327377")));
-        $json_config = json_encode($config, JSON_UNESCAPED_UNICODE);
-        fopen('../../config/' . $phoneid . '_config.json', "w");
-        file_put_contents('../../config/' . $phoneid . '_config.json', $json_config);
-
-        $data=fopen('../../data/' . $phoneid . '_data.json', "w");
-        file_put_contents('../../data/' . $phoneid . '_data.json', $json_data);
-        fwrite($data,"{}");
-
-        echo $phoneid . "的kay为: " . $key . "  添加成功";
+        }else{
+            echo $phoneid . " 已经添加过了";
+            die();
+        }
     }else{
-        echo $phoneid . "/t 已经添加过了";
+        //首次使用创建  初始化文件环境
+        fopen('../../phoneid.json', "w");
+
+        $array_phoneid = array("phoneid" => array($phoneid));
     }
+    file_put_contents('../../phoneid.json', json_encode($array_phoneid, JSON_UNESCAPED_UNICODE));
+    
+    //为新用户创建 config 文件, 并加入测试数据
+    if(!is_dir('../../config')){
+        mkdir('../../config');
+    }
+    $config = array("key" => $key,"items" =>array("75168316327377" => array("description" => "首次添加测试","company" => "yto")));
+    $json_config = json_encode($config, JSON_UNESCAPED_UNICODE);
+    fopen('../../config/' . $phoneid . '_config.json', "w");
+    file_put_contents('../../config/' . $phoneid . '_config.json', $json_config);
+
+    //为新用户创建 data 文件
+    if(!is_dir('../../data')){
+        mkdir('../../data');;
+    }
+    $data=fopen('../../data/' . $phoneid . '_data.json', "w");
+    fwrite($data,"{}");
+
+    echo $phoneid . "的kay为: " . $key . "  添加成功";
+    
 }else{
-		echo "参数不能为空";
+	echo "参数不能为空";
 }
 
 ?>
